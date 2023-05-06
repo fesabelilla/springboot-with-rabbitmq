067fecc91d27f2871bb5d00c5ef1f447074904c9
fesabelilla <fesabelilla@gmail.com>
2023-05-06 20:06:12 +0600

    Spring With RabbitMq init

---
+++ springboot-with-rabbitmq/src/main/java/com/example/springbootwithrabbitmq/SpringbootWithRabbitmqApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootwithrabbitmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWithRabbitmqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootWithRabbitmqApplication.class, args);
+	}
+
+}
---
+++ springboot-with-rabbitmq/src/test/java/com/example/springbootwithrabbitmq/SpringbootWithRabbitmqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootwithrabbitmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootWithRabbitmqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
